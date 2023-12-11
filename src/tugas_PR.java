c5d44a072d46887b34db58d3c02f9b69265d5c03
Rafli Raihan Permana <rafliraihan245@gmail.com>
2023-12-11 19:34:15 +0700

    Update tugas_PR.java

--- src/tugas_PR.java
+++ src/tugas_PR.java
@@ -2,5 +2,7 @@ public class tugas_PR {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("ini adalah file tugas_PR");
+        System.out.println(".");
+        System.out.println("ini perubahan");
     }
 }
